d5fa83fd6abce53bd3a8d05d17616988e5fdc690
Aman00015 <115334661+Aman00015@users.noreply.github.com>
2026-03-28 12:48:43 +0530

    some changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="text-amber-200">
-      <p>Hii</p>
+      <Button>Hey</Button>
     </div>
   );
 }
