4de92a45e79e4a8a59ef1e006e28fc2c8612c84a
Aman00015 <115334661+Aman00015@users.noreply.github.com>
2026-04-03 17:06:08 +0530

    added auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
-
-import {  getQueryClient, trpc } from "@/trpc/server";
-import { Client } from "./client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
-import { Suspense } from "react";
-
+import { requireAuth } from "@/lib/auth-utils";
+import { caller } from "@/trpc/server";
+import { Logout } from "./logout";
 export default async function Page() {
-  const queryClient = getQueryClient();
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
+  await requireAuth();
+  const data = await caller.getUsers();
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center text-amber-200">
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={<p>loading...</p>}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+    <div className="min-h-screen min-w-screen flex items-center justify-center text-black">
+      {JSON.stringify(data)}
+      <Logout />
     </div>
   );
 }
