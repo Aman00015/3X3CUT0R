a2bbcd1e14d91da2082f520270910ec00b9c7f1e
Aman00015 <115334661+Aman00015@users.noreply.github.com>
2026-04-03 18:52:45 +0530

    added styling

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,9 @@
-import { LoginForm } from "@/features/auth/components/login-form"
-import { requireUnAuth } from "@/lib/auth-utils"
+import { LoginForm } from "@/features/auth/components/login-form";
+import { requireUnAuth } from "@/lib/auth-utils";
 
-const Page =async () => {
-await requireUnAuth()
-    return (
-        <div>
-            <LoginForm/>
-        </div>
-    )
-}
-export default Page
+
+const Page = async () => {
+  await requireUnAuth();
+  return <LoginForm />;
+};
+export default Page;
