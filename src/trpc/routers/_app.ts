c230dd1524748ff5e5b6bdb2d869673c00c87ec0
Aman00015 <115334661+Aman00015@users.noreply.github.com>
2026-04-09 22:54:10 +0530

    workflows changes

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,32 +1,13 @@
-import { inngest } from "@/inngest/client";
-import { createTRPCRouter, premiumProcedure, protectedProcedure } from "../init";
-import prisma from "@/lib/db";
-import { google } from "@ai-sdk/google";
-import { generateText } from "ai";
-import { TRPCError } from "@trpc/server";
+import {
+  createTRPCRouter,
+  premiumProcedure,
+  protectedProcedure,
+} from "../init";
+
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-  testAi: protectedProcedure.mutation(async () => {
-
-    await inngest.send({
-      name: "execute/ai",
-    });
-    return { success: true, message: "job queued" };
-  }),
-
-  getWorkflows: protectedProcedure.query(({ ctx }) => {
-    return prisma.workflow.findMany();
-  }),
-
-  createWorkflow: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "app/task.created",
-      data: {
-        id: "task-001",
-      },
-    });
-    return { success: true, message: "job queued" };
-  }),
+  workflows: workflowsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
